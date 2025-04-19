57d52a17b294c0cac0da870176ba1459e23fe3f5
Poulamee Pal <39195528+Polly333@users.noreply.github.com>
2025-04-19 19:49:36 +0530

    POST Request simple

--- FirstSpring/src/main/java/com/udemy/FirstSpring/HelloController.java
+++ FirstSpring/src/main/java/com/udemy/FirstSpring/HelloController.java
@@ -2,6 +2,8 @@ package com.udemy.FirstSpring;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +13,10 @@ public class HelloController {
 
     public String hello(){
         return "You can do it Poulamee!";
+    }
+
+    @PostMapping("/home")
+    public String greet(@RequestBody String name){
+        return "Cheers" + name;
     }
 }
